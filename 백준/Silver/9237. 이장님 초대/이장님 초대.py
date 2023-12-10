c3d4c794d0a8bb76ee29021eafe963292daeb677
hyunn0o <117431437+hyunn0o@users.noreply.github.com>
2023-12-10 18:25:40 +0900

    [Silver V] Title: 이장님 초대, Time: 92 ms, Memory: 42036 KB -BaekjoonHub

--- 백준/Silver/9237. 이장님 초대/이장님 초대.py
+++ 백준/Silver/9237. 이장님 초대/이장님 초대.py
@@ -1,3 +1,6 @@
+# 이장님 초대
+# 백준 9237번
+
 n = int(input())
 days = list(map(int, input().split()))
 
@@ -6,11 +9,11 @@ days.sort(reverse=True)
 max_days = days[0]
 
 if n == 1:
-    print(n + 2)
+    result = max_days + 2
 else:
     for i in range(1, n):
         if max_days < (i + days[i]):
             max_days = i + days[i]
 
     result = max_days + 2
-    print(result)
+print(result)
